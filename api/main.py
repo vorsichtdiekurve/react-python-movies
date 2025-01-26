4f6881371d1538c42e9b093e3fb84a89650144dc
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-01-26 10:53:34 +0100

    Add BE endpoints for handling actors (fetch, add, delete)

--- api/main.py
+++ api/main.py
@@ -10,22 +10,18 @@ import models
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="../ui/build/static", check_dir=False), name="static")
 
-
 @app.get("/")
 def serve_react_app():
     return FileResponse("../ui/build/index.html")
-
 
 @app.get("/movies", response_model=List[schemas.Movie])
 def get_movies():
     return list(models.Movie.select())
 
-
 @app.post("/movies", response_model=schemas.Movie)
 def add_movie(movie: schemas.MovieBase):
     movie = models.Movie.create(**movie.dict())
     return movie
-
 
 @app.get("/movies/{movie_id}", response_model=schemas.Movie)
 def get_movie(movie_id: int):
@@ -34,11 +30,34 @@ def get_movie(movie_id: int):
         raise HTTPException(status_code=404, detail="Movie not found")
     return db_movie
 
-
 @app.delete("/movies/{movie_id}", response_model=schemas.Movie)
-def get_movie(movie_id: int):
+def delete_movie(movie_id: int):
     db_movie = models.Movie.filter(models.Movie.id == movie_id).first()
     if db_movie is None:
         raise HTTPException(status_code=404, detail="Movie not found")
     db_movie.delete_instance()
     return db_movie
+
+@app.get("/actors", response_model=List[schemas.Actor])
+def get_actors():
+    return list(models.Actor.select())
+
+@app.post("/actors", response_model=schemas.Actor)
+def add_actor(actor: schemas.ActorBase):
+    actor = models.Actor.create(**actor.dict())
+    return actor
+
+@app.get("/actors/{actor_id}", response_model=schemas.Actor)
+def get_actor(actor_id: int):
+    db_actor = models.Actor.filter(models.Actor.id == actor_id).first()
+    if db_actor is None:
+        raise HTTPException(status_code=404, detail="Actor not found")
+    return db_actor
+
+@app.delete("/actors/{movie_id}", response_model=schemas.Actor)
+def delete_actor(actor_id: int):
+    db_actor = models.Actor.filter(models.Actor.id == actor_id).first()
+    if db_actor is None:
+        raise HTTPException(status_code=404, detail="Actor not found")
+    db_actor.delete_instance()
+    return db_actor
